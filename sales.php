3f0dbdc7e4f960c024a87ed2982f22d4f6fb7dbd
wnamen <williamjosephnamen@gmail.com>
2016-10-15 11:17:32 -0700

    completed template

---
+++ sales.php
@@ -0,0 +1,54 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <title>LevelUpSeduction</title>
+        
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu:300" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <section id="container">
+            <header id="head-container">
+                <img id="head-image" src="title-image.jpg">
+            </header>
+            
+           
+            <content id="content-container">
+            <h1 class="title-container">Your wish is my command…</h1>
+            <h3 class="title-container">Let me fulfill your every wish and desire with women. Get the type of dating life you want and deserve, with me by your side helping you along the way.</h3>
+                <div class="content-sections"><?php
+                    // A simple web site in Cloud9 that runs through Apache
+                    // Press the 'Run' button on the top to start the web server,
+                    // then click the URL that is emitted to the Output tab of the console
+                
+                    echo 'Vegan bicycle rights jianbing enamel pin fingerstache beard. Activated charcoal fap vegan, tote bag +1 ramps bushwick pok pok wolf distillery everyday carry vaporware sartorial 8-bit. Lyft activated charcoal pinterest, food truck everyday carry brooklyn cray kombucha poutine pabst hammock cornhole. Brooklyn XOXO pop-up humblebrag migas leggings. Gastropub meditation thundercats salvia, hell of tumblr celiac VHS echo park selfies selvage beard subway tile chicharrones. Bushwick before they sold out master cleanse small batch neutra migas, four loko pinterest. Artisan cliche master cleanse semiotics activated.';
+                ?></div>
+                
+                <div class="quotes"><i>"I was never the guy in my group of friends to pick up girls. But after "leveling up" with Jessica, I'm now the guy that all my friends ask, "How the hell do you do it!" Definitely a nice change from being the nice guy who always finished last" - Jeff, 35, Hoboken, NJ</i></div>
+                
+                <div class="content-sections"><?php
+                echo 'Vegan bicycle rights jianbing enamel pin fingerstache beard. Activated charcoal fap vegan, tote bag +1 ramps bushwick pok pok wolf distillery everyday carry vaporware sartorial 8-bit. Lyft activated charcoal pinterest, food truck everyday carry brooklyn cray kombucha poutine pabst hammock cornhole. Brooklyn XOXO pop-up humblebrag migas leggings. Gastropub meditation thundercats salvia, hell of tumblr celiac VHS echo park selfies selvage beard subway tile chicharrones. Bushwick before they sold out master cleanse small batch neutra migas, four loko pinterest. Artisan cliche master cleanse semiotics activated.';
+                ?></div>
+
+                <div class="quotes"><i class="quotes">"I used to be too shy to speak to people in general before Jessica. Now I have an active social life, great friends, and women who somehow just gravitate towards me everywhere I go. I'm still an introvert the only difference is now I'm an introvert that women are attracted to. Jessica knows her stuff I don't know where I'd be without her." Richard, 22, Portland, OR</i></div>
+
+                <div class="content-sections"><?php
+                echo 'Vegan bicycle rights jianbing enamel pin fingerstache beard. Activated charcoal fap vegan, tote bag +1 ramps bushwick pok pok wolf distillery everyday carry vaporware sartorial 8-bit. Lyft activated charcoal pinterest, food truck everyday carry brooklyn cray kombucha poutine pabst hammock cornhole. Brooklyn XOXO pop-up humblebrag migas leggings. Gastropub meditation thundercats salvia, hell of tumblr celiac VHS echo park selfies selvage beard subway tile chicharrones. Bushwick before they sold out master cleanse small batch neutra migas, four loko pinterest. Artisan cliche master cleanse semiotics activated.';
+                ?></div>
+                
+                <div class="quotes"><i class="quotes">"Didn't think I get a number from a very cute girl within 24hrs using what you recommended with using only 3messages, I LOVE JESSICA!!" Dustin, 25 North Dakota</i></div>
+
+                <div class="content-sections"><?php
+                echo 'Vegan bicycle rights jianbing enamel pin fingerstache beard. Activated charcoal fap vegan, tote bag +1 ramps bushwick pok pok wolf distillery everyday carry vaporware sartorial 8-bit. Lyft activated charcoal pinterest, food truck everyday carry brooklyn cray kombucha poutine pabst hammock cornhole. Brooklyn XOXO pop-up humblebrag migas leggings. Gastropub meditation thundercats salvia, hell of tumblr celiac VHS echo park selfies selvage beard subway tile chicharrones. Bushwick before they sold out master cleanse small batch neutra migas, four loko pinterest. Artisan cliche master cleanse semiotics activated .';
+                ?></div>
+                
+            </content>
+                
+            <a id="button-container" href="#"><img id="buy-btn" src="buy_button.png"></a>
+            
+        </section>
+    </body>
+</html>
